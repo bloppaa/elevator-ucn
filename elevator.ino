c6b1d99835ab979a3807429707dcfd36cf92e1c0
Pablo <pablocortes1101@gmail.com>
2024-11-13 16:56:20 -0300

    feat: add unique key to queue
    solo se puede agregar un piso una sola vez a la cola

--- elevator.ino
+++ elevator.ino
@@ -5,12 +5,12 @@ const byte COLS = 4;
 
 char hexaKeys[ROWS][COLS] = {
     {'1', '2', '3', '4'},
-    {'5', '6', '7', '8'},
-    {'9', '0', 'A', 'B'},
-    {'C', 'D', 'E', 'F'}};
+    {'5', '6', 'A', '.'},
+    {'.', '.', '.', '.'},
+    {'.', '.', '.', '.'}};
 
-byte rowPins[ROWS] = {9, 8, 7, 6};
-byte colPins[COLS] = {5, 4, 3, 2};
+byte rowPins[ROWS] = {2, 3, 0, 0};
+byte colPins[COLS] = {4, 5, 6, 7};
 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
@@ -19,6 +19,23 @@ char queue[QUEUE_SIZE];
 int front = 0;
 int rear = -1;
 int itemCount = 0;
+
+bool isQueueFull()
+{
+  return itemCount == QUEUE_SIZE;
+}
+
+bool isInQueue(char data)
+{
+  for (int i = 0; i < itemCount; i++)
+  {
+    if (queue[(front + i) % QUEUE_SIZE] == data)
+    {
+      return true;
+    }
+  }
+  return false;
+}
 
 void enqueue(char data)
 {
@@ -42,11 +59,6 @@ char dequeue()
   }
   itemCount--;
   return data;
-}
-
-bool isQueueFull()
-{
-  return itemCount == QUEUE_SIZE;
 }
 
 void printQueue()
@@ -76,16 +88,25 @@ void loop()
   {
     if (customKey >= '1' && customKey <= '6')
     {
-      if (!isQueueFull())
+      if (!isInQueue(customKey))
       {
-        enqueue(customKey);
-        Serial.print("Enqueued: ");
-        Serial.println(customKey);
-        printQueue();
+        if (!isQueueFull())
+        {
+          enqueue(customKey);
+          Serial.print("Enqueued: ");
+          Serial.println(customKey);
+          printQueue();
+        }
+        else
+        {
+          Serial.println("Queue is full");
+        }
       }
       else
       {
-        Serial.println("Queue is full");
+        Serial.print("Key ");
+        Serial.print(customKey);
+        Serial.println(" is already in the queue");
       }
     }
     else if (customKey == 'A')
---
+++ stepper.ino
@@ -0,0 +1,21 @@
+// #include <Stepper.h>
+
+// const int stepsPerRevolution = 2038;
+
+// Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
+
+// void setup()
+// {
+//     ;
+// }
+
+// void loop()
+// {
+//     myStepper.setSpeed(5);
+//     myStepper.step(stepsPerRevolution);
+//     delay(1000);
+
+//     myStepper.setSpeed(10);
+//     myStepper.step(-stepsPerRevolution);
+//     delay(1000);
+// }
