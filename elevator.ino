de4b19d0b3c8ae3273f48017c85f580a14eca83d
Pablo <pablocortes1101@gmail.com>
2024-11-20 05:02:12 -0300

    feat: press button to move to floor
    gracias chatgpt

--- elevator.ino
+++ elevator.ino
@@ -1,7 +1,6 @@
 #include <Keypad.h>
 #include <Servo.h>
 
-// Keypad Setup
 const byte ROWS = 2;
 const byte COLS = 3;
 
@@ -15,9 +14,9 @@ byte colPins[COLS] = {4, 5, 6};
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 Servo servo;
-int pos = 0;
+int currentPos = 0;
 unsigned long lastServoUpdate = 0;
-int servoDirection = 1;
+int targetPos = 0;
 
 const int QUEUE_SIZE = 6;
 int queue[QUEUE_SIZE];
@@ -25,9 +24,21 @@ int front = 0;
 int rear = -1;
 int itemCount = 0;
 
+int currentFloor = 1;
+const int FLOOR_HEIGHT = 30;
+
+bool delayAtDestination = false;
+unsigned long destinationDelayStart = 0;
+const unsigned long destinationDelay = 2000;
+
 bool isQueueFull()
 {
     return itemCount == QUEUE_SIZE;
+}
+
+bool isQueueEmpty()
+{
+    return itemCount == 0;
 }
 
 bool isInQueue(int data)
@@ -84,12 +95,24 @@ void moveServoNonBlocking()
     unsigned long currentMillis = millis();
     if (currentMillis - lastServoUpdate >= 15)
     {
-        pos += servoDirection;
-        if (pos >= 180 || pos <= 0)
+        if (currentPos < targetPos)
         {
-            servoDirection *= -1;
+            currentPos++;
+            servo.write(currentPos);
         }
-        servo.write(pos);
+        else if (currentPos > targetPos)
+        {
+            currentPos--;
+            servo.write(currentPos);
+        }
+        else
+        {
+            if (!delayAtDestination)
+            {
+                delayAtDestination = true;
+                destinationDelayStart = currentMillis;
+            }
+        }
         lastServoUpdate = currentMillis;
     }
 }
@@ -98,6 +121,7 @@ void setup()
 {
     Serial.begin(9600);
     servo.attach(7);
+    servo.write(0);
 }
 
 void loop()
@@ -106,11 +130,28 @@ void loop()
     if (customKey)
     {
         int customKeyInt = customKey - '0';
-        if (!isInQueue(customKeyInt) && !isQueueFull())
+        if (!isInQueue(customKeyInt) && !isQueueFull() && customKeyInt != currentFloor)
         {
             enqueue(customKeyInt);
             printQueue();
         }
+        delay(200);
+    }
+
+    if (delayAtDestination)
+    {
+        unsigned long currentMillis = millis();
+        if (currentMillis - destinationDelayStart >= destinationDelay)
+        {
+            delayAtDestination = false;
+            if (!isQueueEmpty())
+            {
+                int nextFloor = dequeue();
+                targetPos = (nextFloor - 1) * FLOOR_HEIGHT;
+                currentFloor = nextFloor;
+            }
+        }
+        return;
     }
 
     moveServoNonBlocking();
