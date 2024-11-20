52ce7d846ea96bd4fb4ca2559a2afe7601e21325
Pablo <pablocortes1101@gmail.com>
2024-11-20 17:31:25 -0300

    feat!: (v0.1) working

--- elevator.ino
+++ elevator.ino
@@ -1,158 +1,72 @@
-// #include <Keypad.h>
-// #include <Servo.h>
+#include <Keypad.h>
+#include <Stepper.h>
 
-// const byte ROWS = 2;
-// const byte COLS = 3;
+const byte ROWS = 2;
+const byte COLS = 3;
 
-// char hexaKeys[ROWS][COLS] = {
-//     {'1', '2', '3'},
-//     {'4', '5', '6'}};
+char hexaKeys[ROWS][COLS] = {
+    {'1', '2', '3'},
+    {'4', '5', '6'}};
 
-// byte rowPins[ROWS] = {2, 3};
-// byte colPins[COLS] = {4, 5, 6};
+byte rowPins[ROWS] = {2, 3};
+byte colPins[COLS] = {4, 5, 6};
 
-// Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
-// Servo servo;
-// int currentPos = 0;
-// unsigned long lastServoUpdate = 0;
-// int targetPos = 0;
+const float stepsPerRevolution = 2038;
+Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
 
-// const int QUEUE_SIZE = 6;
-// int queue[QUEUE_SIZE];
-// int front = 0;
-// int rear = -1;
-// int itemCount = 0;
+// Alturas entre pisos
+const float HEIGHTS[] = {0, 8000, 6000, 3000, 3000, 3000}; // Índice 0 no se usa
 
-// int currentFloor = 1;
-// const int FLOOR_HEIGHT = 30;
+float currentFloor = 5;
 
-// bool delayAtDestination = false;
-// unsigned long destinationDelayStart = 0;
-// const unsigned long destinationDelay = 2000;
+void setup()
+{
+    Serial.begin(9600);
+    myStepper.setSpeed(15); // Configura la velocidad del motor
+}
 
-// bool isQueueFull()
-// {
-//     return itemCount == QUEUE_SIZE;
-// }
+void loop()
+{
+    char customKey = customKeypad.getKey();
+    if (customKey)
+    {
+        int customKeyInt = customKey - '0'; // Convierte el carácter en entero
 
-// bool isQueueEmpty()
-// {
-//     return itemCount == 0;
-// }
+        if (customKeyInt >= 1 && customKeyInt <= 6 && customKeyInt != currentFloor)
+        {
+            Serial.print("Moviendo al piso: ");
+            Serial.println(customKeyInt);
 
-// bool isInQueue(int data)
-// {
-//     for (int i = 0; i < itemCount; i++)
-//     {
-//         if (queue[(front + i) % QUEUE_SIZE] == data)
-//         {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
+            // Calcula los pasos necesarios para llegar al piso objetivo
+            float stepsToMove = 0;
 
-// void enqueue(int data)
-// {
-//     if (!isQueueFull())
-//     {
-//         if (rear == QUEUE_SIZE - 1)
-//         {
-//             rear = -1;
-//         }
-//         queue[++rear] = data;
-//         itemCount++;
-//     }
-// }
+            if (customKeyInt > currentFloor)
+            {
+                // Moviéndose hacia arriba
+                for (int i = currentFloor; i < customKeyInt; i++)
+                {
+                    stepsToMove += HEIGHTS[i];
+                }
+            }
+            else
+            {
+                // Moviéndose hacia abajo
+                for (int i = currentFloor - 1; i >= customKeyInt; i--)
+                {
+                    stepsToMove -= HEIGHTS[i];
+                }
+            }
 
-// int dequeue()
-// {
-//     int data = queue[front++];
-//     if (front == QUEUE_SIZE)
-//     {
-//         front = 0;
-//     }
-//     itemCount--;
-//     return data;
-// }
+            // Mueve el motor según los pasos calculados
+            myStepper.step(-stepsToMove);
 
-// void printQueue()
-// {
-//     for (int i = 0; i < itemCount; i++)
-//     {
-//         Serial.print(queue[(front + i) % QUEUE_SIZE]);
-//         if (i < itemCount - 1)
-//         {
-//             Serial.print(", ");
-//         }
-//     }
-//     Serial.println();
-// }
+            // Actualiza el piso actual
+            currentFloor = customKeyInt;
 
-// void moveServoNonBlocking()
-// {
-//     unsigned long currentMillis = millis();
-//     if (currentMillis - lastServoUpdate >= 15)
-//     {
-//         if (currentPos < targetPos)
-//         {
-//             currentPos++;
-//             servo.write(currentPos);
-//         }
-//         else if (currentPos > targetPos)
-//         {
-//             currentPos--;
-//             servo.write(currentPos);
-//         }
-//         else
-//         {
-//             if (!delayAtDestination)
-//             {
-//                 delayAtDestination = true;
-//                 destinationDelayStart = currentMillis;
-//             }
-//         }
-//         lastServoUpdate = currentMillis;
-//     }
-// }
-
-// void setup()
-// {
-//     Serial.begin(9600);
-//     servo.attach(7);
-//     servo.write(0);
-// }
-
-// void loop()
-// {
-//     char customKey = customKeypad.getKey();
-//     if (customKey)
-//     {
-//         int customKeyInt = customKey - '0';
-//         if (!isInQueue(customKeyInt) && !isQueueFull() && customKeyInt != currentFloor)
-//         {
-//             enqueue(customKeyInt);
-//             printQueue();
-//         }
-//         delay(200);
-//     }
-
-//     if (delayAtDestination)
-//     {
-//         unsigned long currentMillis = millis();
-//         if (currentMillis - destinationDelayStart >= destinationDelay)
-//         {
-//             delayAtDestination = false;
-//             if (!isQueueEmpty())
-//             {
-//                 int nextFloor = dequeue();
-//                 targetPos = (nextFloor - 1) * FLOOR_HEIGHT;
-//                 currentFloor = nextFloor;
-//             }
-//         }
-//         return;
-//     }
-
-//     moveServoNonBlocking();
-// }
+            // Detente un momento al llegar al piso
+            delay(500);
+        }
+    }
+}
